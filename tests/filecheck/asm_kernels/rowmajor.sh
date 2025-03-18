a36e8f8bfceb480debece9554fee3960aeb84f4a
Sasha Lopoukhine <superlopuh@gmail.com>
2025-03-18 13:15:11 +0000

    replace asm with dummy check

--- tests/filecheck/asm_kernels/rowmajor.sh
+++
@@ -1,6 +0,0 @@
-# RUN: bash %s %t | filecheck %s
-
-clang -o $1 arm_4x4_matmul_asm_rowmajor/main.c arm_4x4_matmul_asm_rowmajor/test1.s
-$1
-
-# CHECK: Test Passed: The results are equal!
---
+++ tests/filecheck/dummy.sh
@@ -0,0 +1,3 @@
+# RUN: echo "Hello" | filecheck %s
+
+# CHECK: Hello
