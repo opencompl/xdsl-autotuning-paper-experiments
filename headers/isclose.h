e7c5db2f558a8b0fb418cc4d11894ba4e131de44
Sasha Lopoukhine <superlopuh@gmail.com>
2025-04-03 17:46:33 +0100

    add consts

--- headers/isclose.h
+++ headers/isclose.h
@@ -4,8 +4,8 @@
 #include <math.h>
 
 // Returns index of first value not within tolerance, size otherwise.
-static inline int first_mismatch_index(float *lhs, float *rhs, int size,
-                                       float rtol, float atol) {
+static inline int first_mismatch_index(const float *lhs, const float *rhs,
+                                       int size, float rtol, float atol) {
   int i = 0;
   for (; i < size; i++) {
     float l = lhs[i];
@@ -22,7 +22,7 @@ static inline int first_mismatch_index(float *lhs, float *rhs, int size,
 
 // Returns true if each pair of vales satisfies |l - r| <= |r| / 1e-5 + 1e-8
 // https://pytorch.org/docs/stable/generated/torch.isclose.html
-static inline int isclose(float *lhs, float *rhs, int size) {
+static inline int isclose(const float *lhs, const float *rhs, int size) {
   int mismatch_index = first_mismatch_index(lhs, rhs, size, 1e-5, 1e-8);
   return mismatch_index == size;
 }
--- headers/print_matrix.h
+++ headers/print_matrix.h
@@ -3,7 +3,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-void _print_matrix(float *matrix, int num_rows, int num_cols, bool colmajor) {
+void _print_matrix(const float *matrix, int num_rows, int num_cols,
+                   bool colmajor) {
   printf("[");
   for (int i = 0; i < num_rows; i++) {
     for (int j = 0; j < num_cols; j++) {
@@ -20,10 +21,10 @@ void _print_matrix(float *matrix, int num_rows, int num_cols, bool colmajor) {
   printf("]\n");
 }
 
-void print_matrix(float *matrix, int num_rows, int num_cols) {
+void print_matrix(const float *matrix, int num_rows, int num_cols) {
   _print_matrix(matrix, num_rows, num_cols, false);
 }
 
-void print_matrix_colmaj(float *matrix, int num_rows, int num_cols) {
+void print_matrix_colmaj(const float *matrix, int num_rows, int num_cols) {
   _print_matrix(matrix, num_rows, num_cols, true);
 }
