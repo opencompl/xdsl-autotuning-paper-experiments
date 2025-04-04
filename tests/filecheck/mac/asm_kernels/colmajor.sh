b4bb016c205a27ee43f2b2e36fa21da9575c2776
Sasha Lopoukhine <superlopuh@gmail.com>
2025-04-04 13:19:07 +0100

    fill all outputs with seeded data before running matmul

--- tests/filecheck/mac/asm_kernels/colmajor.sh
+++ tests/filecheck/mac/asm_kernels/colmajor.sh
@@ -14,4 +14,48 @@
 # CHECK-NEXT:   2.00, 7.00, 0.00, 7.00;
 # CHECK-NEXT:   5.00, 5.00, 6.00, 2.00]
 
-# CHECK: Test Passed: The results are equal!
+# C
+
+# CHECK-NEXT:  [6.00, 9.00, 3.00, 3.00;
+# CHECK-NEXT:   4.00, 0.00, 5.00, 0.00;
+# CHECK-NEXT:   6.00, 2.00, 4.00, 0.00;
+# CHECK-NEXT:   3.00, 7.00, 8.00, 1.00]
+
+# A_colmaj
+
+# CHECK-NEXT:  [0.00, 3.00, 4.00, 3.00;
+# CHECK-NEXT:   1.00, 4.00, 3.00, 0.00;
+# CHECK-NEXT:   5.00, 4.00, 6.00, 4.00;
+# CHECK-NEXT:   5.00, 4.00, 3.00, 2.00]
+
+# A_colmaj
+
+# CHECK-NEXT:  [8.00, 9.00, 2.00, 5.00;
+# CHECK-NEXT:   3.00, 4.00, 7.00, 5.00;
+# CHECK-NEXT:   8.00, 5.00, 0.00, 6.00;
+# CHECK-NEXT:   6.00, 3.00, 7.00, 2.00]
+
+# A_colmaj
+
+# CHECK-NEXT:  [6.00, 4.00, 6.00, 3.00;
+# CHECK-NEXT:   9.00, 0.00, 2.00, 7.00;
+# CHECK-NEXT:   3.00, 5.00, 4.00, 8.00;
+# CHECK-NEXT:   3.00, 0.00, 0.00, 1.00]
+
+# C
+
+# CHECK-NEXT:  [44.00, 64.00, 35.00, 48.00;
+# CHECK-NEXT:   88.00, 73.00, 68.00, 66.00;
+# CHECK-NEXT:   86.00, 81.00, 65.00, 81.00;
+# CHECK-NEXT:   42.00, 47.00, 36.00, 50.00]
+
+
+# C_colmaj transposed
+
+# CHECK-NEXT:  [44.00, 64.00, 35.00, 48.00;
+# CHECK-NEXT:   88.00, 73.00, 68.00, 66.00;
+# CHECK-NEXT:   86.00, 81.00, 65.00, 81.00;
+# CHECK-NEXT:   42.00, 47.00, 36.00, 50.00]
+
+# CHECK-EMPTY:
+# CHECK-NEXT: Test Passed: The results are equal!
