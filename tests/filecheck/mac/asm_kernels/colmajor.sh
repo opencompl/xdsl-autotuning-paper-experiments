9241ffca318489e948dc330ad3c16f06768925ac
Sasha Lopoukhine <superlopuh@gmail.com>
2025-04-06 15:34:20 +0100

    print matrix names for filecheck (#23)
    This was useful for debugging, so I thought I'd keep it.

--- tests/filecheck/mac/asm_kernels/colmajor.sh
+++ tests/filecheck/mac/asm_kernels/colmajor.sh
@@ -1,48 +1,47 @@
 # RUN: clang -o %t arm_4x4_matmul_asm_colmajor/main.c arm_4x4_matmul_asm_colmajor/test1.s && %t | filecheck %s
+# CHECK:       A
 
-# A
-
-# CHECK:       [0.00, 1.00, 5.00, 5.00;
+# CHECK-NEXT:  [0.00, 1.00, 5.00, 5.00;
 # CHECK-NEXT:   3.00, 4.00, 4.00, 4.00;
 # CHECK-NEXT:   4.00, 3.00, 6.00, 3.00;
 # CHECK-NEXT:   3.00, 0.00, 4.00, 2.00]
 
-# B
+# CHECK-NEXT: B
 
 # CHECK-NEXT:  [8.00, 3.00, 8.00, 6.00;
 # CHECK-NEXT:   9.00, 4.00, 5.00, 3.00;
 # CHECK-NEXT:   2.00, 7.00, 0.00, 7.00;
 # CHECK-NEXT:   5.00, 5.00, 6.00, 2.00]
 
-# C
+# CHECK-NEXT: C
 
 # CHECK-NEXT:  [6.00, 9.00, 3.00, 3.00;
 # CHECK-NEXT:   4.00, 0.00, 5.00, 0.00;
 # CHECK-NEXT:   6.00, 2.00, 4.00, 0.00;
 # CHECK-NEXT:   3.00, 7.00, 8.00, 1.00]
 
-# A_colmaj
+# CHECK-NEXT: A_colmaj
 
 # CHECK-NEXT:  [0.00, 3.00, 4.00, 3.00;
 # CHECK-NEXT:   1.00, 4.00, 3.00, 0.00;
 # CHECK-NEXT:   5.00, 4.00, 6.00, 4.00;
 # CHECK-NEXT:   5.00, 4.00, 3.00, 2.00]
 
-# A_colmaj
+# CHECK-NEXT: B_colmaj
 
 # CHECK-NEXT:  [8.00, 9.00, 2.00, 5.00;
 # CHECK-NEXT:   3.00, 4.00, 7.00, 5.00;
 # CHECK-NEXT:   8.00, 5.00, 0.00, 6.00;
 # CHECK-NEXT:   6.00, 3.00, 7.00, 2.00]
 
-# A_colmaj
+# CHECK-NEXT: C_colmaj
 
 # CHECK-NEXT:  [6.00, 4.00, 6.00, 3.00;
 # CHECK-NEXT:   9.00, 0.00, 2.00, 7.00;
 # CHECK-NEXT:   3.00, 5.00, 4.00, 8.00;
 # CHECK-NEXT:   3.00, 0.00, 0.00, 1.00]
 
-# C
+# CHECK-NEXT: C out
 
 # CHECK-NEXT:  [44.00, 64.00, 35.00, 48.00;
 # CHECK-NEXT:   88.00, 73.00, 68.00, 66.00;
@@ -50,7 +49,7 @@
 # CHECK-NEXT:   42.00, 47.00, 36.00, 50.00]
 
 
-# C_colmaj transposed
+# CHECK-NEXT:  C_asm out
 
 # CHECK-NEXT:  [44.00, 64.00, 35.00, 48.00;
 # CHECK-NEXT:   88.00, 73.00, 68.00, 66.00;
--- tests/filecheck/mac/asm_kernels/rowmajor.sh
+++ tests/filecheck/mac/asm_kernels/rowmajor.sh
@@ -1,27 +1,27 @@
 # RUN: clang -o %t arm_4x4_matmul_asm_rowmajor/main.c arm_4x4_matmul_asm_rowmajor/test1.s && %t | filecheck %s
 
-# A
+# CHECK:       A
 
-# CHECK:       [0.00, 1.00, 5.00, 5.00;
+# CHECK-NEXT:  [0.00, 1.00, 5.00, 5.00;
 # CHECK-NEXT:   3.00, 4.00, 4.00, 4.00;
 # CHECK-NEXT:   4.00, 3.00, 6.00, 3.00;
 # CHECK-NEXT:   3.00, 0.00, 4.00, 2.00]
 
-# B
+# CHECK-NEXT: B
 
 # CHECK-NEXT:  [8.00, 3.00, 8.00, 6.00;
 # CHECK-NEXT:   9.00, 4.00, 5.00, 3.00;
 # CHECK-NEXT:   2.00, 7.00, 0.00, 7.00;
 # CHECK-NEXT:   5.00, 5.00, 6.00, 2.00]
 
-# C
+# CHECK-NEXT: C
 
 # CHECK-NEXT:  [6.00, 9.00, 3.00, 3.00;
 # CHECK-NEXT:   4.00, 0.00, 5.00, 0.00;
 # CHECK-NEXT:   6.00, 2.00, 4.00, 0.00;
 # CHECK-NEXT:   3.00, 7.00, 8.00, 1.00]
 
-# C
+# CHECK-NEXT: C out
 
 # CHECK-NEXT:  [44.00, 64.00, 35.00, 48.00;
 # CHECK-NEXT:   88.00, 73.00, 68.00, 66.00;
@@ -29,7 +29,7 @@
 # CHECK-NEXT:   42.00, 47.00, 36.00, 50.00]
 
 
-# C_asm
+# CHECK-NEXT: C_asm
 
 # CHECK-NEXT:  [44.00, 64.00, 35.00, 48.00;
 # CHECK-NEXT:   88.00, 73.00, 68.00, 66.00;
