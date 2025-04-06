fbc1f0fd6e9083bb044f6e08f1dda2df475ef05c
Sasha Lopoukhine <superlopuh@gmail.com>
2025-04-06 15:29:17 +0100

    print matrix names for filecheck

--- arm_4x4_matmul_asm_colmajor/main.c
+++ arm_4x4_matmul_asm_colmajor/main.c
@@ -43,24 +43,33 @@ int main() {
   fill_random_data(B, SIZE * SIZE);
   fill_random_data(C, SIZE * SIZE);
 
+  printf("A\n");
   print_matrix(A, SIZE, SIZE);
+  printf("B\n");
   print_matrix(B, SIZE, SIZE);
+  printf("C\n");
   print_matrix(C, SIZE, SIZE);
 
   transpose(A, A_colmaj);
   transpose(B, B_colmaj);
   transpose(C, C_colmaj);
 
+  printf("A_colmaj\n");
   print_matrix(A_colmaj, SIZE, SIZE);
+  printf("B_colmaj\n");
   print_matrix(B_colmaj, SIZE, SIZE);
+  printf("C_colmaj\n");
   print_matrix(C_colmaj, SIZE, SIZE);
 
   matrix_mul_4x4_ref(C, A, B);
   matrix_mul_4x4_asm(C_colmaj, A_colmaj, B_colmaj);
 
+  printf("C out\n");
   print_matrix(C, SIZE, SIZE);
 
   float res[SIZE * SIZE];
+
+  printf("C_asm out\n");
   transpose(C_colmaj, res);
 
   print_matrix(res, SIZE, SIZE);
@@ -70,10 +79,6 @@ int main() {
     return 0;
   } else {
     printf("\nTest Failed: The results do not match.\n");
-    printf("Matrix C (ref):\n");
-    print_matrix(C, SIZE, SIZE);
-    printf("\nMatrix C (asm):\n");
-    print_matrix_colmaj(C_colmaj, SIZE, SIZE);
     return 1;
   }
 }
