8a5149d4ab807e16c9393a075b4dc103143c675e
Sasha Lopoukhine <superlopuh@gmail.com>
2025-10-07 09:18:05 +0100

    fix ttile plot title (#176)

--- src/autotuner/plot_ttile.py
+++ src/autotuner/plot_ttile.py
@@ -15,6 +15,14 @@ def plot_flops_per_time(df: pd.DataFrame, output_file: Path | None = None):
     # Calculate FLOPs per time (throughput)
     valid_data["throughput"] = valid_data["flops"] / valid_data["time"]
 
+    ns = set(df.N)
+    ks = set(df.K)
+    dtypes = set(df["dtype"])
+    assert len(ns) == len(ks) == len(dtypes) == 1
+    (n,) = ns
+    (k,) = ks
+    (dtype,) = dtypes
+
     fig, ax = plt.subplots(figsize=(8, 6))
 
     # Assign a color and marker for each variant
@@ -40,9 +48,7 @@ def plot_flops_per_time(df: pd.DataFrame, output_file: Path | None = None):
 
     ax.set_xlabel("M")
     ax.set_ylabel("Throughput (FLOPs per Time)")
-    ax.set_title(
-        "Performance of small matrix multiplication kernels, for N = K = 128 and 8 ≤ M ≤ 50"
-    )
+    ax.set_title(f"Performance of small matrix multiplication kernels, for N = K = {n}")
     ax.grid(True, alpha=0.3)
     ax.set_xlim(0, valid_data["M"].max() + 2)
     ax.set_ylim(bottom=1e-2)  # Avoid log(0); adjust as needed for your data
