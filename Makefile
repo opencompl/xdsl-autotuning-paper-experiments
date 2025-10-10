c5e58f608d259b8f2c30852a9a06a3d3629c2e0a
emmau678 <emmau678@gmail.com>
2025-10-10 15:46:54 +0100

    change how files are generated

--- Makefile
+++ Makefile
@@ -48,18 +48,24 @@ PLOTS += plots/ttile.f64.neon.png
 PLOTS += plots/cube_8.f64.neon.png
 PLOTS += plots/cube_16.f64.neon.png
 PLOTS += plots/cube_64.f64.neon.png
+PLOTS += plots/tiny.f64.neon.png
+PLOTS += plots/heatmap.f64.neon.png
 
 PLOTS += plots/ttile.f32.tower.png
 PLOTS += plots/ttile.f64.tower.png
 PLOTS += plots/cube_8.f64.tower.png
 PLOTS += plots/cube_16.f64.tower.png
 PLOTS += plots/cube_64.f64.tower.png
+PLOTS += plots/tiny.f64.tower.png
+PLOTS += plots/heatmap.f64.tower.png
 
 PLOTS += plots/ttile.f32.pinocchio.png
 PLOTS += plots/ttile.f64.pinocchio.png
 PLOTS += plots/cube_8.f64.pinocchio.png
 PLOTS += plots/cube_16.f64.pinocchio.png
 PLOTS += plots/cube_64.f64.pinocchio.png
+PLOTS += plots/tiny.f64.pinocchio.png
+PLOTS += plots/heatmap.f64.pinocchio.png
 
 plots/ttile.%.png: data/ttile.%.jsonl src/autotuner/plot_ttile.py
 	uv run plot-ttile $< --output $@
@@ -70,9 +76,11 @@ plots/cube%.png: data/cube%.jsonl src/autotuner/plot_cube.py
 plots/bars.%.png: data/bars.%.jsonl src/autotuner/plot_cube.py
 	uv run plot-cube $< --output $@
 
-.PHONY: plot-small-matrices
-plot-small-matrices: data/small_matrix.f64.tower.jsonl src/autotuner/plot_small_matrices.py
-	uv run plot-small-matrices $< --output plots/small_matrices
+plots/tiny.%.png: data/small_matrix.%.jsonl src/autotuner/plot_small_matrices.py
+	uv run plot-tiny-line $< --output $@
+
+plots/heatmap.%.png: data/small_matrix.%.jsonl src/autotuner/plot_small_matrices.py
+	uv run plot-tiny-heatmap $< --output $@
 
 .PHONY: plots
 plots: $(PLOTS)
--- src/autotuner/plot_small_matrices.py
+++ src/autotuner/plot_small_matrices.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 
 def plot_flops_per_time(
-    df: pd.DataFrame, m: int | None, output_dir: Path | None = None
+    df: pd.DataFrame, title: str, xlabel: str, output_path: Path | None = None
 ):
     """Plot FLOPs per time for each kernel variant."""
 
@@ -43,16 +43,6 @@ def plot_flops_per_time(
         )
 
     ax.set_ylabel("Throughput (FLOPs per Time)")
-    if m is not None:
-        ax.set_title(
-            f"Performance of small matrix multiplication kernels, for M = {m}, K = 64 and 1 ≤ N ≤ 16"
-        )
-        ax.set_xlabel("N")
-    else:
-        ax.set_title(
-            "Performance of small square matrix multiplication kernels, for M = N, K = 64 and 1 ≤ M,N ≤ 16"
-        )
-        ax.set_xlabel("M, N")
     ax.grid(True, alpha=0.3)
     ax.set_xlim(0, valid_data["N"].max() + 2)
     ax.set_ylim(bottom=1e-2)  # Avoid log(0); adjust as needed for your data
@@ -61,18 +51,16 @@ def plot_flops_per_time(
     ax.legend(title="Variant")
     plt.tight_layout()
 
-    if output_dir:
-        os.makedirs(output_dir, exist_ok=True)
-        if m is not None:
-            output_path = os.path.join(output_dir, f"plot_{m}xNx64.png")
-        else:
-            output_path = os.path.join(output_dir, "plot_MxNx64.png")
+    if output_path:
+        os.makedirs(output_path.parent, exist_ok=True)
         plt.savefig(output_path, dpi=300, bbox_inches="tight")
     else:
         plt.show()
 
 
-def plot_heatmap_throughput_over_peak(df: pd.DataFrame, output_dir: Path | None = None):
+def plot_heatmap_throughput_over_peak(
+    df: pd.DataFrame, output_path: Path | None = None
+):
     """Plot a heatmap of throughput over peak perf for small matrices."""
 
     peaks = df["peak"].dropna().unique()
@@ -123,15 +111,14 @@ def plot_heatmap_throughput_over_peak(df: pd.DataFrame, output_dir: Path | None 
         "Performance of small square matrix multiplication kernels, for 1 ≤ M ≤ 16, 1 ≤ N ≤ 16, K = 64"
     )
 
-    if output_dir:
-        os.makedirs(output_dir, exist_ok=True)
-        output_path = os.path.join(output_dir, "heatmap.png")
+    if output_path:
+        os.makedirs(output_path.parent, exist_ok=True)
         plt.savefig(output_path, dpi=300, bbox_inches="tight")
     else:
         plt.show()
 
 
-def main():
+def main(heatmap: bool):
     import argparse
 
     parser = argparse.ArgumentParser(
@@ -148,16 +135,38 @@ def main():
 
     df = pd.read_json(args.input, lines=True)
 
+    if heatmap:
+        heatmap_df = df[df["variant"] == "libxsmm"]
+        plot_heatmap_throughput_over_peak(df=heatmap_df, output_path=args.output)
+        return
+
     for m, group in df.groupby("M"):
         assert isinstance(m, int)
-        plot_flops_per_time(group, m, output_dir=args.output)
+        if args.output is not None:
+            tiny_path = args.output.parent / "small_matrices" / f"plot_{m}xNx64.png"
+            os.makedirs(tiny_path.parent, exist_ok=True)
+        else:
+            tiny_path = None
+        plot_flops_per_time(
+            group,
+            title=f"Performance of small matrix multiplication kernels, for M = {m}, K = 64 and 1 ≤ N ≤ 16",
+            xlabel="N",
+            output_path=tiny_path,
+        )
 
     square_matrices = df[df["M"] == df["N"]]
-    plot_flops_per_time(square_matrices, m=None, output_dir=args.output)
+    assert isinstance(square_matrices, pd.DataFrame)
+    plot_flops_per_time(
+        square_matrices,
+        title="Performance of small square matrix multiplication kernels, for M = N, K = 64 and 1 ≤ M,N ≤ 16",
+        xlabel="M,N",
+        output_path=args.output,
+    )
 
-    heatmap_df = df[df["variant"] == "libxsmm"]
-    plot_heatmap_throughput_over_peak(df=heatmap_df, output_dir=args.output)
+
+def main_heatmap():
+    main(heatmap=True)
 
 
-if __name__ == "__main__":
-    main()
+def main_line():
+    main(heatmap=False)
