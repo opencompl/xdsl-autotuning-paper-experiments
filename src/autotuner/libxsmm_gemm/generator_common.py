3626702fa2a24bee3f8ee957c181a7e77a3a33de
Sasha Lopoukhine <superlopuh@gmail.com>
2025-10-19 17:08:55 +0100

    implement function signature generation

---
+++ src/autotuner/libxsmm_gemm/generator_common.py
@@ -0,0 +1,20 @@
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.dialects.x86 import registers
+from xdsl.ir import Block, Region
+from xdsl.dialects.x86_func import FuncOp, RetOp
+from xdsl.rewriter import InsertPoint, Rewriter
+
+
+def libxsmm_mmfunction_signature(module: ModuleOp, routine_name: str) -> FuncOp:
+    operand_types = (registers.RDI, registers.RSI, registers.RDX)
+
+    func = FuncOp(
+        routine_name,
+        Region(Block((RetOp(),), arg_types=operand_types)),
+        (operand_types, ()),
+        "public",
+    )
+
+    Rewriter.insert_op(func, InsertPoint.at_end(module.body.block))
+
+    return func
--- src/autotuner/libxsmm_gemm/generator_gemm.py
+++ src/autotuner/libxsmm_gemm/generator_gemm.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.dialects.x86.ops import print_assembly
+from xdsl.dialects.x86_func import FuncOp
+from xdsl.ir import Block, Region
+
+from autotuner.libxsmm_gemm.generator_common import libxsmm_mmfunction_signature
 from autotuner.libxsmm_gemm.libxsmm_cpuid import Arch
 from autotuner.libxsmm_gemm.libxsmm_main import GEMMDescriptor
 
@@ -7,4 +13,19 @@ from autotuner.libxsmm_gemm.libxsmm_main import GEMMDescriptor
 def libxsmm_generator_gemm_directasm(
     file_out: Path, routine_name: str, desc: GEMMDescriptor, arch: Arch
 ):
+    module_op = ModuleOp(Region(Block()))
+
+    # Instead of function signature, generate `FuncOp`.
+    func_op = libxsmm_mmfunction_signature(module_op, routine_name)
+
+    # Generate the actual kernel code for current description depending on the
+    # architecture.
+    libxsmm_generator_gemm_kernel(func_op, arch, desc)
+
+    # Append code to source file
+    with open(file_out, "rw") as f:
+        print_assembly(module_op, f)
+
+
+def libxsmm_generator_gemm_kernel(func_op: FuncOp, arch: Arch, desc: GEMMDescriptor):
     raise NotImplementedError
