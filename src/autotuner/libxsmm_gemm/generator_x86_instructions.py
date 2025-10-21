09209acd256e2b27747318a959b4910b2c7d047b
Sasha Lopoukhine <superlopuh@gmail.com>
2025-10-21 20:48:52 +0100

    port prologue code

--- src/autotuner/libxsmm_gemm/generator_x86_instructions.py
+++ src/autotuner/libxsmm_gemm/generator_x86_instructions.py
@@ -1,3 +1,4 @@
+from xdsl.dialects import x86
 from autotuner.libxsmm_gemm.generator_common import GPRegMapping
 from autotuner.libxsmm_gemm.libxsmm_generator import GeneratedCode
 from autotuner.libxsmm_gemm.libxsmm_main import GEMMPrefetchType
@@ -9,156 +10,18 @@ def libxsmm_x86_instruction_open_stream_gemm(
     skip_callee_save: bool,
     prefetch: GEMMPrefetchType,
 ) -> None:
-    raise NotImplementedError
+    args = generated_code.func_op.body.block.args
+    a, b, c = args
 
+    builder = generated_code.builder
 
-# LIBXSMM_API_INTERN
-# void libxsmm_x86_instruction_open_stream_gemm( libxsmm_generated_code*       io_generated_code,
-#                                                const libxsmm_gp_reg_mapping* i_gp_reg_mapping,
-#                                                const unsigned int            skip_callee_save,
-#                                                unsigned int                  i_prefetch) {
-#   /* TODO: add checks in debug mode */
-#   if ( io_generated_code->code_type > 1 ) {
-#     /* TODO: this is currently System V AMD64 RTL(C) ABI only */
-#     unsigned char* l_code_buffer = (unsigned char *) io_generated_code->generated_code;
-#     unsigned int l_code_size = io_generated_code->code_size;
-#     unsigned int l_max_size = io_generated_code->buffer_size;
+    builder.insert(x86.ops.DS_MovOp(a, destination=gp_reg_mapping.gp_reg_a))
+    builder.insert(x86.ops.DS_MovOp(b, destination=gp_reg_mapping.gp_reg_b))
+    builder.insert(x86.ops.DS_MovOp(c, destination=gp_reg_mapping.gp_reg_c))
 
-#     if (NULL == l_code_buffer || l_max_size < (l_code_size + 9)) {
-#       LIBXSMM_HANDLE_ERROR(io_generated_code, LIBXSMM_ERR_BUFFER_TOO_SMALL);
-#       return;
-#     }
-
-#     if ( skip_callee_save == 0 ) {
-#       /* on windows we also have to save xmm6-xmm15 */
-# #if defined(_WIN32) || defined(__CYGWIN__)
-#       unsigned int l_i;
-#       unsigned int l_simd_store_instr = (io_generated_code->arch < LIBXSMM_X86_AVX) ? LIBXSMM_X86_INSTR_MOVUPS_ST
-#                                                                                     : LIBXSMM_X86_INSTR_VMOVUPS_ST;
-#       /* decrease rsp by 160 (10x16) */
-#       libxsmm_x86_instruction_alu_imm(io_generated_code, LIBXSMM_X86_INSTR_SUBQ, LIBXSMM_X86_GP_REG_RSP, 160);
-#       /* save 10 xmm onto the stack */
-#       for (l_i = 0; l_i < 10; ++l_i) {
-#         libxsmm_x86_instruction_vec_compute_mem_1reg_mask(io_generated_code,
-#           l_simd_store_instr, 'x',
-#           LIBXSMM_X86_GP_REG_RSP, LIBXSMM_X86_GP_REG_UNDEF, 0, 144 - (l_i * 16), 0, 6 + l_i, 0, 0);
-#       }
-#       /* update code length */
-#       l_code_size = io_generated_code->code_size;
-# #endif
-#       /* push callee save registers */
-#       /* push rbx */
-#       l_code_buffer[l_code_size++] = 0x53;
-#       /* push r12 */
-#       l_code_buffer[l_code_size++] = 0x41;
-#       l_code_buffer[l_code_size++] = 0x54;
-#       /* push r13 */
-#       l_code_buffer[l_code_size++] = 0x41;
-#       l_code_buffer[l_code_size++] = 0x55;
-#       /* push r14 */
-#       l_code_buffer[l_code_size++] = 0x41;
-#       l_code_buffer[l_code_size++] = 0x56;
-#       /* push r15 */
-#       l_code_buffer[l_code_size++] = 0x41;
-#       l_code_buffer[l_code_size++] = 0x57;
-# #if defined(_WIN32) || defined(__CYGWIN__)
-#       /* push rdi */
-#       l_code_buffer[l_code_size++] = 0x57;
-#       /* push rsi */
-#       l_code_buffer[l_code_size++] = 0x56;
-# #endif
-#       /* update code length */
-#       io_generated_code->code_size = l_code_size;
-#     }
-#   } else if ( io_generated_code->code_type == 1 ) {
-#     /* TODO: this is currently System V AMD64 RTL(C) ABI only */
-#     char l_new_code[512];
-#     int l_max_code_length = 511;
-#     int l_code_length = 0;
-
-#     if ( skip_callee_save == 0 ) {
-#       /* on windows we also have to save xmm6-xmm15 */
-# #if defined(_WIN32) || defined(__CYGWIN__)
-#       {
-#         unsigned int l_i;
-#         unsigned int l_simd_store_instr = (io_generated_code->arch < LIBXSMM_X86_AVX) ? LIBXSMM_X86_INSTR_MOVUPS_LD
-#                                                                                       : LIBXSMM_X86_INSTR_VMOVUPS_LD;
-#         char l_gp_reg_base_name[4];
-#         char l_instr_name[16];
-
-#         libxsmm_get_x86_gp_reg_name(LIBXSMM_X86_GP_REG_RSP, l_gp_reg_base_name, 3);
-
-#         /* decrease rsp by 160 (10x16) */
-#         libxsmm_get_x86_instr_name(LIBXSMM_X86_INSTR_SUBQ, l_instr_name, 15);
-#         l_code_length = LIBXSMM_SNPRINTF(
-#           l_new_code, l_max_code_length, "                       %s $%i, %%%s\n", l_instr_name, 160, l_gp_reg_base_name);
-#         libxsmm_append_code_as_string(io_generated_code, l_new_code, l_code_length);
-
-#         libxsmm_get_x86_instr_name(l_simd_store_instr, l_instr_name, 15);
-#         /* save 10 xmm onto the stack */
-#         for (l_i = 0; l_i < 10; ++l_i) {
-#           l_code_length = LIBXSMM_SNPRINTF(l_new_code, l_max_code_length, "                       %s %%%cmm%u, %i(%%%s)\\n\\t\"\n",
-#             l_instr_name, 'x', 6 + l_i, 144 - (l_i * 16), l_gp_reg_base_name);
-#           libxsmm_append_code_as_string(io_generated_code, l_new_code, l_code_length);
-#         }
-#       }
-# #endif
-#       /* push callee save registers */
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%rbx\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%r12\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%r13\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%r14\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%r15\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-# #if defined(_WIN32) || defined(__CYGWIN__)
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%rdi\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       pushq %%rsi\n" );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-# #endif
-#     }
-
-#     l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       retq\n" );
-#     libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#   } else {
-#     char l_new_code[512];
-#     int l_max_code_length = 511;
-#     int l_code_length = 0;
-#     char l_gp_reg_name[4];
-
-#     /* loading a pointer in assembly */
-#     libxsmm_get_x86_gp_reg_name( i_gp_reg_mapping->gp_reg_a, l_gp_reg_name, 3 );
-#     l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "  __asm__ __volatile__(\"movq %%0, %%%%%s\\n\\t\"\n", l_gp_reg_name );
-#     libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-
-#     /* loading b pointer in assembly */
-#     libxsmm_get_x86_gp_reg_name( i_gp_reg_mapping->gp_reg_b, l_gp_reg_name, 3 );
-#     l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       \"movq %%1, %%%%%s\\n\\t\"\n", l_gp_reg_name );
-#     libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-
-#     /* loading c pointer in assembly */
-#     libxsmm_get_x86_gp_reg_name( i_gp_reg_mapping->gp_reg_c, l_gp_reg_name, 3 );
-#     l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       \"movq %%2, %%%%%s\\n\\t\"\n", l_gp_reg_name );
-#     libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-
-#     /* loading b prefetch pointer in assembly */
-#     if ( i_prefetch == LIBXSMM_GEMM_PREFETCH_BL2) {
-#       libxsmm_get_x86_gp_reg_name( i_gp_reg_mapping->gp_reg_b_prefetch, l_gp_reg_name, 3 );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       \"movq %%3, %%%%%s\\n\\t\"\n", l_gp_reg_name );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#     /* loading a prefetch pointer in assembly */
-#     } else if ( i_prefetch == LIBXSMM_GEMM_PREFETCH_AL2 ) {
-#       libxsmm_get_x86_gp_reg_name( i_gp_reg_mapping->gp_reg_a_prefetch, l_gp_reg_name, 3 );
-#       l_code_length = LIBXSMM_SNPRINTF( l_new_code, l_max_code_length, "                       \"movq %%3, %%%%%s\\n\\t\"\n", l_gp_reg_name );
-#       libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
-#     } else {}
-
-#   }
-# }
+    match prefetch:
+        case GEMMPrefetchType.BL2 | GEMMPrefetchType.AL2:
+            raise NotImplementedError
 
 
 def libxsmm_x86_instruction_close_stream_gemm(
