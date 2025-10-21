58a65721a75f25c2a19dad893efe011a7313c851
Sasha Lopoukhine <superlopuh@gmail.com>
2025-10-21 22:15:43 +0100

    add Arch to GeneratedCode

--- src/autotuner/libxsmm_gemm/libxsmm_generator.py
+++ src/autotuner/libxsmm_gemm/libxsmm_generator.py
@@ -3,8 +3,11 @@ from dataclasses import dataclass
 from xdsl.builder import Builder
 from xdsl.dialects.x86_func import FuncOp
 
+from autotuner.libxsmm_gemm.libxsmm_cpuid import Arch
+
 
 @dataclass
 class GeneratedCode:
     func_op: FuncOp
     builder: Builder
+    arch: Arch
