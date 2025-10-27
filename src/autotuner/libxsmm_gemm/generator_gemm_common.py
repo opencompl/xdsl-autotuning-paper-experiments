7d5acb41bd41e4468278487fe74338f89c0ab2d3
Sasha Lopoukhine <superlopuh@gmail.com>
2025-10-27 08:58:07 +0000

    fix flags in gemm_footer_mloop

--- src/autotuner/libxsmm_gemm/generator_gemm_common.py
+++ src/autotuner/libxsmm_gemm/generator_gemm_common.py
@@ -1116,40 +1116,40 @@ def libxsmm_generator_gemm_footer_mloop(
 
     # Also adjust eltwise pointers
     if (
-        micro_kernel_config.fused_relu == 1
-        or micro_kernel_config.vnni_cvt_output_ext_buf == 1
-        or micro_kernel_config.fused_relu_bwd == 1
-        or micro_kernel_config.fused_bcolbias == 1
-        or micro_kernel_config.fused_hcolbias == 1
-        or micro_kernel_config.fused_b8colbias == 1
-        or micro_kernel_config.fused_h8colbias == 1
-        or micro_kernel_config.fused_scolbias == 1
-        or micro_kernel_config.overwrite_C == 0
+        micro_kernel_config.fused_relu
+        or micro_kernel_config.vnni_cvt_output_ext_buf
+        or micro_kernel_config.fused_relu_bwd
+        or micro_kernel_config.fused_bcolbias
+        or micro_kernel_config.fused_hcolbias
+        or micro_kernel_config.fused_b8colbias
+        or micro_kernel_config.fused_h8colbias
+        or micro_kernel_config.fused_scolbias
+        or not micro_kernel_config.overwrite_C
     ):
         raise NotImplementedError
 
-    if micro_kernel_config.fused_relu == 1 and micro_kernel_config.overwrite_C == 1:
+    if micro_kernel_config.fused_relu and micro_kernel_config.overwrite_C:
         raise NotImplementedError
 
-    if micro_kernel_config.vnni_cvt_output_ext_buf == 1:
+    if micro_kernel_config.vnni_cvt_output_ext_buf:
         raise NotImplementedError
 
-    if micro_kernel_config.fused_relu_bwd == 1:
+    if micro_kernel_config.fused_relu_bwd:
         raise NotImplementedError
 
-    if not micro_kernel_config.overwrite_C == 0:
+    if not micro_kernel_config.overwrite_C:
         raise NotImplementedError
 
     if micro_kernel_config.fused_bcolbias or micro_kernel_config.fused_hcolbias:
         raise NotImplementedError
 
-    if micro_kernel_config.fused_b8colbias == 1:
+    if micro_kernel_config.fused_b8colbias:
         raise NotImplementedError
 
-    if micro_kernel_config.fused_h8colbias == 1:
+    if micro_kernel_config.fused_h8colbias:
         raise NotImplementedError
 
-    if micro_kernel_config.fused_scolbias == 1:
+    if micro_kernel_config.fused_scolbias:
         raise NotImplementedError
 
     if (
