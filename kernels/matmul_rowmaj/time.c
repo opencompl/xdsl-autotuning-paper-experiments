1495c5647b9092654c23f509cdab4956e4606fc4
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-11-24 16:10:32 +0100

    driver: externalize the monotonic clock vs hw counters logic (#192)

--- kernels/matmul_rowmaj/time.c
+++ kernels/matmul_rowmaj/time.c
@@ -9,45 +9,13 @@
 
 #include "../../headers/gendata.h"
 #include "../../headers/mnk.h"
+#include "../../headers/perf.h"
 
 #define TARGET_SIZE 268435456
 #define NUM_ITERATIONS (TARGET_SIZE / (M * N * K))
 #define CLOCKS_PER_USEC ((double)CLOCKS_PER_SEC / 1000000.0)
 
 extern void matmul(DTYPE A[M * K], DTYPE B[K * N], DTYPE C[M * N]);
-
-#ifdef USE_PAPI
-#include <papi.h>
-#endif
-
-#define CHECK(call)                                                            \
-  do {                                                                         \
-    int __ret = (call);                                                        \
-    if (__ret != PAPI_OK) {                                                    \
-      fprintf(stderr, #call " failed: %s\n", PAPI_strerror(__ret));            \
-      exit(1);                                                                 \
-    }                                                                          \
-  } while (0)
-
-int hardware_counters_available(void) {
-  int res = 0;
-#ifdef USE_PAPI
-  // Check the availability of the event
-  int av = PAPI_query_event(PAPI_TOT_CYC);
-  if (av == PAPI_OK) {
-    // Check the level of paranoid
-    FILE *f = fopen("/proc/sys/kernel/perf_event_paranoid", "r");
-    if (f) {
-      int val;
-      if (fscanf(f, "%d", &val) == 1) {
-        res = (val <= 2);
-      }
-      fclose(f);
-    }
-  }
-#endif
-  return res;
-}
 
 int main() {
 
@@ -56,26 +24,8 @@ int main() {
   posix_memalign((void **)&B, 64, K * N * sizeof(DTYPE));
   posix_memalign((void **)&C, 64, M * N * sizeof(DTYPE));
 
-  long long values[1];
-  struct timespec ts_start;
-  struct timespec ts_end;
-  double elapsed;
-
-#ifdef USE_PAPI
-  int ret = PAPI_library_init(PAPI_VER_CURRENT);
-  if (ret != PAPI_VER_CURRENT) { fprintf(stderr, "papi init failed\n"); return 1; }
-  int EventSet = PAPI_NULL;
-  CHECK(PAPI_create_eventset(&EventSet));
-#endif
-
-  int hw_counters = hardware_counters_available();
-
-  if (hw_counters) {
-#ifdef USE_PAPI
-    CHECK(PAPI_add_event(EventSet, PAPI_TOT_CYC));
-#endif
-  }
-
+  time_init();
+  
   set_random_seed(42);
 
   fill_random_data(A, M * K);
@@ -87,32 +37,16 @@ int main() {
   // Warm up the cache with one iteration
   matmul(A, B, C);
 
-  // Start the counters
-  if (hw_counters) {
-#ifdef USE_PAPI
-    CHECK(PAPI_start(EventSet));
-#endif
-  } else {
-    clock_gettime(CLOCK_MONOTONIC, &ts_start);
-  }
-
+  time_start();
+  
   for (int i = 0; i < NUM_ITERATIONS; i++) {
     matmul(A, B, C);
   }
-
-  // Stop the counters
-  if (hw_counters) {
-#ifdef USE_PAPI
-    CHECK(PAPI_stop(EventSet, values));
-    elapsed = (double)values[0];
-#endif
-  } else {
-    clock_gettime(CLOCK_MONOTONIC, &ts_end);
-    elapsed = (ts_end.tv_nsec - ts_start.tv_nsec) * (double)FREQ;
-  }
+  
+  double elapsed = time_end(FREQ);
 
   double average_cycles = (double)elapsed / (double)NUM_ITERATIONS;
-  printf("%f\n", average_cycles);
+  printf("%lf\n", average_cycles);
 
   free(A);
   free(B);
