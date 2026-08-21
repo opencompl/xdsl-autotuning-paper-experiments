9937506bc5ab4fc7c436748aa8ee96fd20a2ecdb
Sasha Lopoukhine <superlopuh@gmail.com>
2026-08-21 10:40:31 +0200

    add helper to convert Datatype to builtin types (#282)

--- src/autotuner/libxsmm_gemm/libxsmm_typedefs.py
+++ src/autotuner/libxsmm_gemm/libxsmm_typedefs.py
@@ -1,5 +1,7 @@
 from enum import StrEnum, auto
 
+from xdsl.dialects.builtin import Float32Type, Float64Type, f32, f64
+
 
 class Datatype(StrEnum):
     # Order preserved from libxsmm
@@ -19,3 +21,15 @@ class Datatype(StrEnum):
     U8 = auto()
     IMPLICIT = auto()
     UNSUPPORTED = auto()
+
+    @property
+    def builtin_type(self) -> Float32Type | Float64Type:
+        match self:
+            case Datatype.F32:
+                return f32
+            case Datatype.F64:
+                return f64
+            case _:
+                raise NotImplementedError(
+                    f"Unsupported builtin floating-point type for {self}"
+                )
